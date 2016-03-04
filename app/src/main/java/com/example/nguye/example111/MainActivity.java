f1ca4cf665ceae89fb453b98f33486906b6bea2e
tuan nguyen <tuan nguyen>
2016-03-04 16:50:36 +0700

    chu i2

--- app/src/main/java/com/example/nguye/example111/MainActivity.java
+++ app/src/main/java/com/example/nguye/example111/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.nguye.example111;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn=(Button)findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                
-            }
-        });
+
     }
 }
